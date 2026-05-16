6aa78a5bb4e654707914d7cca84ed5105ff9fc83
Hamad77S <surour77h@gmail.com>
2026-05-16 14:01:33 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='football-games-total-orange-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
